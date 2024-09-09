dee32709c1c70f31a231ab1048672ace2484ee70
mazurp2 <pawel.mazur@contractors.roche.com>
2024-09-09 18:44:56 +0200

    Added tests for homepage

--- locators/locators.py
+++ locators/locators.py
@@ -1,12 +1,11 @@
-from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
-import allure
 
 
 class HomePageLocators:
     add_remove_elements_tab = (By.XPATH, "//a[text()='Add/Remove Elements']")
     checkboxes_tab = (By.XPATH, "//a[text()='Checkboxes']")
     context_menu_tab = (By.XPATH, "//a[text()='Context Menu']")
+    dropdown_tab = (By.XPATH, "//a[text()='Dropdown']")
 
 
 class InternalPages:
@@ -14,3 +13,4 @@ class InternalPages:
     checkbox1 = (By.XPATH, "//input[1][@type=\"checkbox\"]")
     checkbox2 = (By.XPATH, "//input[2][@type=\"checkbox\"]")
     context_box = (By.ID, "hot-spot")
+    dropdown = (By.ID, "dropdown")
